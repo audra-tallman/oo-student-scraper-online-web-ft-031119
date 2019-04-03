3d2542213e65fcfb57c3158e294b8fc000836dac
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 03:25:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,11 @@ require 'Nokogiri'
 require 'pry'
 
 class Scraper
+  attr_reader :name, :location, :profile_url
+
 
   def self.scrape_index_page(index_url)
-    
+    :name
   end
 
   def self.scrape_profile_page(profile_url)

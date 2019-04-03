e5911d61ca81cec1ffc66d624a713486f609fa77
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 04:11:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash
+    student_hash.each {|attribute, value|
+      self.send("#{attribute}=", value)}
     @@all << self
   end
 

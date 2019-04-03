938fdf35bdd7deb172690412e60033bb2637eed4
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 04:05:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,7 +33,7 @@ class Scraper
         end 
     end 
 
-  student[:profile_quote] =
+  student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
   student[:bio] = 
 end
 

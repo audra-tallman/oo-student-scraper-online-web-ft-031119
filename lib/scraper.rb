e43fd0e2e5555eba9374efa9e5cc96e648358c81
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 03:59:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,8 +18,9 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
-    
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map {item
   end
 
 end

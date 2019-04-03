2c60b360dd18d4ea92e7694490df5592edc83ce3
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 03:37:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,13 @@ class Scraper
   attr_accessor :studend_index_array, :index_url, :profile_url
 
 
+  def initialize
+    @student_index_array = {:name => [], :location => [], :profile_url =>[]}
+  end 
+
+
   def self.scrape_index_page(index_url)
-    @info = {:name => [], :location => [], :profile_url =>[]}
+    @student_index_array.all.map {|student| (student_index_array[0], student_index_array[1], student_index_array[2])}
   end
 
   def self.scrape_profile_page(profile_url)

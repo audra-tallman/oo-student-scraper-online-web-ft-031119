666f62f8160c02d78a98b2ffa1780ee54b53d244
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 04:07:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,5 +35,6 @@ class Scraper
 
     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-end
+
+  student
 end

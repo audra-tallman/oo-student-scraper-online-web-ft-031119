9b1b679343d9d5221b7f970ee09785d29b9e8098
Audra Tallman <audi.tallman@gmail.com>
2019-04-03 04:03:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,18 @@ class Scraper
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").children.css("a").map {|item| item.attribute('href').value}
-  end
+    links.each do |link|
+        if link.include?("linkedin")
+            student[:linkedin] = link
+        elsif link.include?('github')
+            student[:github] = link
+        elsif link.include?("twitter")
+            student[:twitter] = link
+        else
+            student[:blog] = link
+        end 
+    end 
+
 
 end
 
